7ebfb540ed6d67b7dd3ef28b8a1479ba22ebc295
Varun <var17t@gmail.com>
2025-01-28 16:04:40 +0530

    fix: more changes headers

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -21,7 +21,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://rtc-chat-client.vercel.app/"],
+    origin: ["http://localhost:3000", "https://rtc-chat-client.vercel.app"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Access-Control-Allow-Origin"],
   }
